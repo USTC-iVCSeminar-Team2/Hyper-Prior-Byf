3554f1e0b8f74c57d9259597de18a98f54fbfc1d
EsakaK <togelbian@gmail.com>
2022-07-24 15:12:34 +0800

    finish all

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,3 +6,4 @@ from .analysis_hyper import Analysis_Hyper_Net
 from .synthesis_hyper import Synthesis_Hyper_Net
 from .EntropyModelGaussion import EntropyModelGaussion
 from .entropyCoder import EntropyCoder, EntropyCoderGaussian
+from .gdn0 import GDN0
--- modules/synthesis.py
+++ modules/synthesis.py
@@ -33,13 +33,13 @@ class Synthesis_Net(nn.Module):
             math.sqrt(2 * (3 + self.num_channel_N) / (self.num_channel_N + self.num_channel_N))))
         torch.nn.init.constant_(self.deconv4.bias.data, 0.01)
 
-        self.igdn1 = GDN(self.num_channel_N, inverse=True)
-        self.igdn2 = GDN(self.num_channel_N, inverse=True)
-        self.igdn3 = GDN(self.num_channel_N, inverse=True)
+        self.iGDN1 = GDN(self.num_channel_N, inverse=True)
+        self.iGDN2 = GDN(self.num_channel_N, inverse=True)
+        self.iGDN3 = GDN(self.num_channel_N, inverse=True)
 
     def forward(self, inputs):
-        x3 = self.igdn1(self.deconv1(inputs))
-        x2 = self.igdn2(self.deconv2(x3))
-        x1 = self.igdn3(self.deconv3(x2))
+        x3 = self.iGDN1(self.deconv1(inputs))
+        x2 = self.iGDN2(self.deconv2(x3))
+        x1 = self.iGDN3(self.deconv3(x2))
         x_hat = self.deconv4(x1)  # x_hat in shape of [B,3,256,256]
         return x_hat
